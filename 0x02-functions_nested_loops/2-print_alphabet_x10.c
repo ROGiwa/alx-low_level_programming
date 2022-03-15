e2f9e6146034a88289a02816a7bb8a7b4aa72406
Rilwan Giwa <giwarilwan00@gmail.com>
2022-03-15 22:14:16 +0100

    alpha x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 /**
  *print_alphabet_x10 - print alpha x10
  *Description: alpha x10.
- *
  *Return: no return value.
  */
 void print_alphabet_x10(void)
@@ -10,15 +9,16 @@ void print_alphabet_x10(void)
       char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
       int i, j;
       i = 0;
-   while (i < 10)
-{
-      j = 0;
-      while (j < 26)
-{
-      _putchar(alphabet[j]);
-      j++;
-}
-      _putchar('\n');
-      i++;
-}
+
+      while (i < 10)
+      {
+            j = 0;
+            while (j < 26)
+            {
+                   _putchar(alphabet[j]);
+                   j++;
+            }
+            _putchar('\n');
+            i++;
+            }
 }
