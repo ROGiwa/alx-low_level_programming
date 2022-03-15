db72c2f1eed6565e2694176af2c16d61fd4c941d
Rilwan Giwa <giwarilwan00@gmail.com>
2022-03-15 20:53:45 +0100

    print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,12 @@
  */
 void print_alphabet(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz"
 	int i;
-	for (i = 0; i < 26; i++)
+
+	for (i = 'a'; i <= 'z'; i++)
 
 	{
-		_putchar(alphabet[i]);
+		_putchar(i);
 	}
-
-	         _putchar('\n');
+	_putchar('\n')
 }
