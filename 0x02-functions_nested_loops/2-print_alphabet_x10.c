2114dc8311c138ed4217407fd2e2fa92ce55b56c
Rilwan Giwa <giwarilwan00@gmail.com>
2022-03-15 22:34:18 +0100

    lower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,25 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- *print_alphabet_x10 - print alpha x10
- *Description: alpha x10.
- *Return: no return value.
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
  */
+
 void print_alphabet_x10(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i, j;
-	i = 0;
+	char c;
+	int i = 0;
 
 	while (i < 10)
 	{
-        	j = 0;
-        	while (j < 26)
-        	{	
-        		_putchar(alphabet[j]);
-        		j++;
-            	}
-            	_putchar('\n');
-            	i++;
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
 	}
+
 }
